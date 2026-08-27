9bb3ebfe790a56cd0b73e72eb85100b1424f6e45
kamruj-akash <source.akash@gmail.com>
2026-08-27 23:04:48 +0600

    feat: enhance appointment, doctor, patient, and schedule models with new relationships and unique constraints

---
+++ prisma/migrations/20260827165817_init/migration.sql
@@ -0,0 +1,60 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[doctorId,patientId,scheduleId]` on the table `appointments` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[scheduleId,serialNumber,joiningTime]` on the table `appointments` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `doctorId` to the `appointments` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `patientId` to the `appointments` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `scheduleId` to the `appointments` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateEnum
+CREATE TYPE "ScheduleStatus" AS ENUM ('DRAFT', 'PUBLISHED');
+
+-- AlterTable
+ALTER TABLE "appointments" ADD COLUMN     "doctorId" TEXT NOT NULL,
+ADD COLUMN     "joiningTime" TIMESTAMP(3),
+ADD COLUMN     "patientId" TEXT NOT NULL,
+ADD COLUMN     "prescriptionUrl" JSONB,
+ADD COLUMN     "recordUrl" JSONB[],
+ADD COLUMN     "scheduleId" TEXT NOT NULL,
+ADD COLUMN     "serialNumber" INTEGER;
+
+-- CreateTable
+CREATE TABLE "schedules" (
+    "id" TEXT NOT NULL,
+    "startDateTime" TIMESTAMP(3) NOT NULL,
+    "endDateTime" TIMESTAMP(3) NOT NULL,
+    "totalSlot" INTEGER NOT NULL,
+    "availableSlot" INTEGER NOT NULL,
+    "meetingLink" TEXT NOT NULL,
+    "status" "ScheduleStatus" NOT NULL DEFAULT 'DRAFT',
+    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+    "deletedAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "doctorId" TEXT NOT NULL,
+
+    CONSTRAINT "schedules_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "schedules_doctorId_startDateTime_endDateTime_key" ON "schedules"("doctorId", "startDateTime", "endDateTime");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "appointments_doctorId_patientId_scheduleId_key" ON "appointments"("doctorId", "patientId", "scheduleId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "appointments_scheduleId_serialNumber_joiningTime_key" ON "appointments"("scheduleId", "serialNumber", "joiningTime");
+
+-- AddForeignKey
+ALTER TABLE "appointments" ADD CONSTRAINT "appointments_patientId_fkey" FOREIGN KEY ("patientId") REFERENCES "patients"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "appointments" ADD CONSTRAINT "appointments_doctorId_fkey" FOREIGN KEY ("doctorId") REFERENCES "doctors"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "appointments" ADD CONSTRAINT "appointments_scheduleId_fkey" FOREIGN KEY ("scheduleId") REFERENCES "schedules"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "schedules" ADD CONSTRAINT "schedules_doctorId_fkey" FOREIGN KEY ("doctorId") REFERENCES "doctors"("id") ON DELETE CASCADE ON UPDATE CASCADE;
